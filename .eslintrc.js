71a6fd1438e1721af634d8de495d3c9db2081a73
정민우 <juty9026@gmail.com>
2021-11-18 00:12:38 +0900

    configure eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  root: true,
+  globals: {
+    _: true,
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'import'],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb-typescript', 'airbnb/hooks', 'prettier', 'plugin:prettier/recommended'],
+  rules: {
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/interactive-supports-focus': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-underscore-dangle': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/no-this-in-sfc': 'off',
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [],
+        extensions: ['.ts', '.tsx'],
+      },
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+};
