452769aa21a8ebe72818cd03be60dfc4525d3357
정민우 <juty9026@gmail.com>
2021-11-21 22:36:18 +0900

    Remove Divider

--- src/components/Divider/Divider.emotion.js
+++
@@ -1,8 +0,0 @@
-import { css } from '@emotion/react';
-
-const divider = css`
-  border-top: 5px solid #bbb;
-  border-radius: 5px;
-`;
-
-export { divider };
--- src/components/Divider/Divider.tsx
+++
@@ -1,9 +0,0 @@
-/** @jsxImportSource @emotion/react */
-import { divider } from './Divider.emotion';
-
-type DividerProps = {};
-const Divider = ({}: DividerProps) => {
-  return <div css={divider} />;
-};
-
-export default Divider;
