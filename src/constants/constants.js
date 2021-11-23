8c411383501efd18047b6f19a3b4b79141968c38
정민우 <juty9026@gmail.com>
2021-11-23 23:59:00 +0900

    Remove constants

--- src/constants/constants.js
+++
@@ -1,9 +0,0 @@
-function define(name, value) {
-  Object.defineProperty(exports, name, {
-    value,
-    enumerable: true,
-  });
-}
-
-define('PARTNER_SKCC', 'skcc');
-define('TECH_STACK_JAVA', 'java');
