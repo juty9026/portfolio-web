3df8368eeba14c13003ac782fc925ef5a1bd1d1c
정민우 <juty9026@gmail.com>
2021-11-26 23:45:49 +0900

    Remove default CRA-App header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import data from '@data';
 import Article from '@components/Article';
@@ -11,15 +10,6 @@ import WorkExperience from '@section/WorkExperience';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
       <main>
         <Article title="About Me">
           <AboutMe data={data.aboutMe} />
