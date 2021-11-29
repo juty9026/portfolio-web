63e185141aa6ab2a55677a42b881f3404d703765
정민우 <juty9026@gmail.com>
2021-11-29 17:16:53 +0900

    Add web font loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import WebFont from 'webfontloader';
 import './App.css';
 import data from '@data';
 import {
@@ -10,16 +12,31 @@ import {
 import Hero from '@components/Hero';
 
 function App() {
+  const [fontLoadded, setFontLoadded] = React.useState(false);
+
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Bebas Neue', 'Jua'],
+      },
+      active: () => setFontLoadded(true),
+    });
+  }, []);
+
   return (
     <div className="App">
-      <main>
-        <Hero />
-        <SkillsArticle />
-        <AboutMeArticle data={data.aboutMe} />
-        <AbilitiesArticle data={data.abilities} />
-        <IntroductionArticle data={data.introduction} />
-        <WorkExperienceArticle data={data.workExperience} />
-      </main>
+      {
+        fontLoadded ? (
+          <main>
+            <Hero />
+            <AboutMeArticle data={data.aboutMe} />
+            <SkillsArticle />
+            <AbilitiesArticle data={data.abilities} />
+            <IntroductionArticle data={data.introduction} />
+            <WorkExperienceArticle data={data.workExperience} />
+          </main>
+        ) : null // TODO: Loading Component
+      }
     </div>
   );
 }
