af94f080eaac843e954a5489390696d066311813
정민우 <juty9026@gmail.com>
2021-12-01 03:01:40 +0900

    Add Profile card data

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -10,6 +10,10 @@ const Profile: React.FC = () => {
         <span css={styles.cardRowContent}>정민우</span>
       </div>
       <div css={styles.cardRow}>
+        <h4 css={styles.cardRowTitle}>Job</h4>
+        <span css={styles.cardRowContent}>Web Developer</span>
+      </div>
+      <div css={styles.cardRow}>
         <h4 css={styles.cardRowTitle}>Email</h4>
         <span css={styles.cardRowContent}>juty9026@gmail.com</span>
       </div>
