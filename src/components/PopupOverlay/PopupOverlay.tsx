5149ba01532e245c77d8a8a0a54f2c1d10016d27
정민우 <juty9026@gmail.com>
2021-12-02 22:37:23 +0900

    Feat PopupOverlay component

---
+++ src/components/PopupOverlay/PopupOverlay.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Wrap = styled.div`
+  position: fixed;
+  display: block;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 20;
+  cursor: pointer;
+  overflow-y: scroll;
+`;
+
+const Container = styled.div`
+  margin: 5vh 10vw 5vh 10vw;
+  background-color: white;
+  font-size: 50px;
+  min-height: 200vh;
+  color: black;
+  cursor: auto;
+  z-index: 30;
+`;
+
+interface Props {
+  onClickOutside?: React.MouseEventHandler;
+  onKeyDown?: (ev: KeyboardEvent) => any;
+}
+
+const PopupOverlay: React.FC<Props> = ({ onClickOutside, onKeyDown, children }) => {
+  React.useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    if (onKeyDown) window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      document.body.style.overflow = 'auto';
+      if (onKeyDown) window.removeEventListener('keydown', onKeyDown);
+    };
+  }, []);
+
+  return (
+    <Wrap
+      id="overlay"
+      onClick={(e) => {
+        if (onClickOutside && e.currentTarget === e.target) {
+          onClickOutside(e);
+        }
+      }}
+    >
+      <Container>{children}</Container>
+    </Wrap>
+  );
+};
+
+export default PopupOverlay;
