385b729039475f29b3f241b493b39719870a563e
정민우 <juty9026@gmail.com>
2021-12-07 22:45:17 +0900

    Modify P02_LCAP content

--- src/components/Article/WorkExperienceArticle/P02_LCAP.tsx
+++ src/components/Article/WorkExperienceArticle/P02_LCAP.tsx
@@ -31,7 +31,7 @@ const P02_LCAP: React.FC = () => {
             모니터링 & 운영 업무를 중점적으로 진행하다보니 반복되는 작업이 많아 개선해야 할 포인트가 눈에 들어왔고, 쉘 스크립트와 자바 어플리케이션을 사용해 업무 자동화를 위한 프로그램을 만들었습니다.
           `}
         </Description>
-        <TechStack backend={['Hadoop', 'Hive', 'Spring', 'JSP', 'Shell Scripting']} />
+        <TechStack backend={['Hadoop', 'Hive', 'Spring', 'JSP']} devops={['Shell Scripting']} />
         <Description>
           {`
             자동화를 하고 나니 업무에 할당하는 시간을 대폭 줄이는 것 외에도 휴먼 에러를 방지하는 효과도 있어 업무 효율이 크게 올랐습니다.
