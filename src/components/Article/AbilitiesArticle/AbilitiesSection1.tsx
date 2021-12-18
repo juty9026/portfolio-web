c524ea73418aaf00f60d81afd7d203b0de30f1c4
정민우 <juty9026@gmail.com>
2021-12-19 01:30:05 +0900

    Refactor AbilitiesSection1.tsx

--- src/components/Article/AbilitiesArticle/AbilitiesSection1.tsx
+++ src/components/Article/AbilitiesArticle/AbilitiesSection1.tsx
@@ -4,7 +4,6 @@ import styled from '@emotion/styled';
 import { FadeSimple, IAmPossible, NoProblem } from '@components/Animated';
 import { Waypoint } from 'react-waypoint';
 import StickyBox from 'react-sticky-box';
-import * as styles from '@styles';
 
 const Wrap = styled.section`
   width: 100vw;
@@ -29,6 +28,12 @@ const Half = styled.div`
   padding: 0 5vw;
 `;
 
+const LeftHalfContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 const H3 = styled.h3`
   display: inline-block;
   color: #fff;
@@ -41,6 +46,7 @@ const P = styled.p`
 `;
 
 const FlexStickyBox = styled(StickyBox)`
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -50,7 +56,7 @@ const AbilitiesSection1: React.FC = () => {
   const [changeWord, setChangeWord] = useState(false);
   return (
     <Wrap>
-      <FadeSimple direction={'top'} bottomOffset="60%">
+      <FadeSimple direction={'top'} bottomOffset="50%">
         <H3>자타공인 해결사.</H3>
       </FadeSimple>
 
@@ -58,14 +64,16 @@ const AbilitiesSection1: React.FC = () => {
 
       <FlexContainer>
         <Half>
-          <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-            <FadeSimple direction="right">
-              <P>
-                어디에서 문제가 생길까요? 원인이 발생하는 포인트를 감각적으로 찾아낼 수 있습니다. 확실하게 아닌 부분을
-                소거하고, 가능성이 높은 부분부터 확인해보면 답은 금방 나오게 되죠.
-              </P>
-            </FadeSimple>
-          </div>
+          <LeftHalfContainer>
+            <FlexStickyBox>
+              <FadeSimple direction="right">
+                <P>
+                  어디에서 문제가 생길까요? 원인이 발생하는 포인트를 감각적으로 찾아낼 수 있습니다. 확실하게 아닌 부분을
+                  소거하고, 가능성이 높은 부분부터 확인해보면 답은 금방 나오게 되죠.
+                </P>
+              </FadeSimple>
+            </FlexStickyBox>
+          </LeftHalfContainer>
           <Waypoint
             onEnter={({ previousPosition }) => {
               setChangeWord(previousPosition === 'below');
@@ -76,19 +84,21 @@ const AbilitiesSection1: React.FC = () => {
             topOffset="45%"
             bottomOffset="45%"
           />
-          <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-            <FadeSimple direction="right">
-              <P>
-                원인을 찾았으니 해결해봅시다. 잘못 구현되어 원하지 않는 결과가 나온것이라면 로직을 수정하고, 추가적인
-                기능이 필요하다면 적절한 위치에 끼워 넣으면 돼요. 하지만, 아무리 급해도 사이드 이펙트가 발생할 수 있는
-                부분은 꼼꼼하게 확인해야 합니다!
-              </P>
-            </FadeSimple>
-            {/*</TextContainer>*/}
-          </div>
+
+          <FlexStickyBox>
+            <LeftHalfContainer>
+              <FadeSimple direction="right">
+                <P>
+                  원인을 찾았으니 해결해봅시다. 잘못 구현되어 원하지 않는 결과가 나온것이라면 로직을 수정하고, 추가적인
+                  기능이 필요하다면 적절한 위치에 끼워 넣으면 돼요. 하지만, 아무리 급해도 사이드 이펙트가 발생할 수 있는
+                  부분은 꼼꼼하게 확인해야 합니다!
+                </P>
+              </FadeSimple>
+            </LeftHalfContainer>
+          </FlexStickyBox>
         </Half>
         <Half>
-          <FlexStickyBox css={styles.fullHeight}>
+          <FlexStickyBox>
             <FadeSimple direction="left" style={{ textAlign: 'left', marginLeft: '5vw' }}>
               <NoProblem change={changeWord} />
             </FadeSimple>
