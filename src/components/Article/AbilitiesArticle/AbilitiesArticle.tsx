95f0a4add349463e5d6821cb07e5ee30fd262c05
정민우 <juty9026@gmail.com>
2021-12-25 23:27:22 +0900

    Modify content (AbilitiesArticle.tsx)

--- src/components/Article/AbilitiesArticle/AbilitiesArticle.tsx
+++ src/components/Article/AbilitiesArticle/AbilitiesArticle.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import {
   SectionCleanCode,
   SectionCommunication,
@@ -7,39 +6,13 @@ import {
   SectionWorkEfficiency,
 } from '@components/Article/AbilitiesArticle/section';
 
-const Wrap = styled.div`
-  width: 100%;
-  margin: auto;
-`;
-
-const H3 = styled.h3`
-  font-size: 4rem;
-`;
-
-const H5 = styled.h5`
-  font-size: 2rem;
-`;
-
-const SubSection = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-`;
-
 const AbilitiesArticle: React.FC = () => {
   return (
     <article>
-      <Wrap>
-        <SectionCommunication />
-        <SectionProblemSolver />
-        <SectionCleanCode />
-        <SectionWorkEfficiency />
-
-        <SubSection>
-          <H5>놀라운 적응력</H5>
-          <H5>강한 개선 의지</H5>
-          <H5>멈추지 않는 성장</H5>
-        </SubSection>
-      </Wrap>
+      <SectionCommunication />
+      <SectionProblemSolver />
+      <SectionCleanCode />
+      <SectionWorkEfficiency />
     </article>
   );
 };
