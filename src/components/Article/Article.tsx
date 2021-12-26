e50e65b6e9b036b73dfcb6b7c43888bb91f1dab6
정민우 <juty9026@gmail.com>
2021-12-26 23:15:08 +0900

    Cleanup Article.tsx

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,30 +1,23 @@
-/** @jsxImportSource @emotion/react */
 import React from 'react';
-import styles from './Article.e';
+import styled from '@emotion/styled';
 
-// import StickyHeader from '@components/StickyHeader';
+const StyledArticle = styled.article`
+  min-height: 100vh;
+`;
 
 interface Props {
-  title: string;
-  fullHeight?: boolean;
+  title?: string;
 }
-
-const Article: React.FC<Props> = ({ title, fullHeight = false, children }) => {
-  const id = React.useMemo(() => {
-    return title.toLowerCase().replace(/ /g, '-');
-  }, [title]);
-
-  const containerStyle = React.useMemo(() => {
-    return fullHeight ? [styles.container, styles.fullHeight] : styles.container;
-  }, [fullHeight]);
-
+const Article: React.FC<Props> = ({ title, children }) => {
   return (
-    <article id={id} css={containerStyle}>
-      <header css={styles.titleContainer}>
-        <h2 css={styles.title}>{title}</h2>
-      </header>
+    <StyledArticle>
+      {!!title && (
+        <header>
+          <h1>{title}</h1>
+        </header>
+      )}
       {children}
-    </article>
+    </StyledArticle>
   );
 };
 
