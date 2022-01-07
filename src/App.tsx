7ff8d2b03d38168799f365b230d1dae71f62255b
정민우 <juty9026@gmail.com>
2022-01-07 13:08:02 +0900

    Remove fontReady state

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import WebFont from 'webfontloader';
 import './App.css';
-import { AbilitiesArticle, AboutMeArticle, SkillsArticle, ProjectsArticle } from '@components/Article';
+import { AbilitiesArticle, AboutMeArticle, ProjectsArticle, SkillsArticle } from '@components/Article';
 import Hero from '@components/Hero';
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 function App() {
-  const [fontReady, setFontReady] = useState(false);
-
   useEffect(() => {
     WebFont.load({
       google: {
         families: ['Bebas Neue', 'Noto Sans', 'Vujahday Script'],
       },
-      active: () => setFontReady(true),
     });
   }, []);
 
   return (
     <ParallaxProvider>
       <div className="App">
-        {
-          fontReady ? (
-            <>
-              <Hero />
-              <main>
-                <AboutMeArticle />
-                <SkillsArticle />
-                <AbilitiesArticle />
-                <ProjectsArticle />
-              </main>
-            </>
-          ) : null // TODO: Loading Component
-        }
+        <Hero />
+        <main>
+          <AboutMeArticle />
+          <SkillsArticle />
+          <AbilitiesArticle />
+          <ProjectsArticle />
+        </main>
       </div>
     </ParallaxProvider>
   );
