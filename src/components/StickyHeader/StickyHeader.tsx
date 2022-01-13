c2bcc33095dc0a99eca93850db1ca4032bd95588
정민우 <juty9026@gmail.com>
2022-01-13 10:34:14 +0900

    refactor: remove unused files

--- src/components/StickyHeader/StickyHeader.tsx
+++
@@ -1,7 +0,0 @@
-/** @jsxImportSource @emotion/react */
-import React from 'react';
-import { sticky } from './StickyHeader.e';
-
-const StickyHeader: React.FC = ({ children }) => <header css={sticky}>{children}</header>;
-
-export default StickyHeader;
